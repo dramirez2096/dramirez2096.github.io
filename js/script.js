60599c8dbb198bf043b6f07b1d9c9e1e59df0785
David Ramirez <dramirez2096@gmail.com>
2017-07-29 20:28:06 -0700

    wd section finished

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Menu Toggle Script
+$("#menu-toggle").click( function (e){
+    e.preventDefault();
+    $("#wrap").toggleClass("menuDisplayed");
+});
+
+// Hamburger Menu Script
+(function() {
+
+"use strict";
+
+var toggles = document.querySelectorAll(".c-hamburger");
+
+for (var i = toggles.length - 1; i >= 0; i--) {
+var toggle = toggles[i];
+toggleHandler(toggle);
+};
+
+function toggleHandler(toggle) {
+toggle.addEventListener( "click", function(e) {
+  e.preventDefault();
+  (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+});
+}
+
+})();
